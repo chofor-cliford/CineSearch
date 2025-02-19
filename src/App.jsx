54a4c6cd6e4b5a7923a76f2b43224ffb8a53e772
Chofor Cliford <betterfuture032@gmail.com>
2025-02-19 15:00:23 -0500

    Add trending movies feature and update UI elements

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
-import { updateSearchCount } from "../appwrite";
+import { getTrendingMovies, updateSearchCount } from "../appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -20,6 +20,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debounceSearchTerm, setDebounceSearchTerm] = useState("");
 
@@ -55,7 +56,6 @@ const App = () => {
       if (query && data.results.length > 0) {
         updateSearchCount(query, data.results[0]);
       }
-
     } catch (error) {
       console.error("Error fetching movies: ", error);
       setErrorMessage("Error fetching movies. Please try again later.");
@@ -64,20 +64,47 @@ const App = () => {
     }
   };
 
+  const loadTrendingMovies = async () => {
+    try {
+      const trendingMovies = await getTrendingMovies();
+      setTrendingMovies(trendingMovies);
+    } catch (error) {
+      console.error("Error fetching trending movies: ", error);
+    }
+  };
+
   useEffect(() => {
     fetchMovies(debounceSearchTerm);
   }, [debounceSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <div className="pattern">
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="Hero Banner" />
+          <img src="./hero-img.png" alt="Hero Banner" />
           <h1>
-            Find <span className="text-gradient">Movies</span> You'll Enjoy
-            without the Hassle
+            Discover <span className="text-gradient">Movies</span> You'll Love.
           </h1>
         </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, idx) => (
+                <li key={movie.$id}>
+                  <p>{idx + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
 
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <section className="all-movies">
